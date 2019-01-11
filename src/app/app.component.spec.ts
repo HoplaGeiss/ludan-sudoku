35a416749140a71e8a848465b29422efc91c69a6
Gabriel Muller <hopla@ifilax.home>
2019-01-11 15:22:27 +0000

    first deploy on firebase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ludan-base'`, () => {
+  it(`should have as title 'ludan-sudoku'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ludan-base');
+    expect(app.title).toEqual('ludan-sudoku');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ludan-base!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ludan-sudoku!');
   });
 });
